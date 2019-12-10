bf087b8a6d65bf7cb0d32336b8372dee1363a8d6
harry-sparta <ka.hei.harry@gmail.com>
2019-12-10 10:11:00 +0000

    passed tests

--- test_unittest_simplecalc.py
+++ test_unittest_simplecalc.py
@@ -6,14 +6,14 @@ class Calctest(unittest.TestCase):
     Calc = SimpleCalc()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(2, 4), 6)
+        self.assertEqual(self.Calc.add(2, 4), 6)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.Calc.subtract(4, 2), 2)
 
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.Calc.multiply(2, 3), 6)
 
     def test_divide(self):
-        self.assertEqual(self.calc.divide(8, 4), 2)
+        self.assertEqual(self.Calc.divide(8, 4), 2)
 
